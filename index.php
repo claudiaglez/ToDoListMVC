678557346182339232d083aff836749f9de262aa
Claudia González <claudia.glezg@gmail.com>
2024-01-31 21:39:31 +0100

    update create

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    require_once("./view/head/head.php");
+?>
+
+
+
+<a href="./view/task/create.php" class="btn btn-primary">Agregar nueva tarea</a>
+
+
+
+<?php
+    require_once("./view/head/footer.php");
+?>
--- view/task/create.php
+++ view/task/create.php
@@ -1,20 +1,24 @@
 <?php
+    require_once("../../view/head/head.php");
 ?>
 <form action="store.php" method="POST" autocomplete="off">
-<div>
+<div class="mb-3">
         <label for="Title" class="form-label">Nueva tarea</label>
-        <input type="text" name="Title" required class="form-control" id="Title">
+        <input type="text" name="Title" required class="form-control" id="Title" aria-describedby="emailHelp">
 
         <label for="Description" class="form-label">Descripción</label>
-        <input type="text" name="Description" required class="form-control" id="Description">
+        <input type="text" name="Description" required class="form-control" id="Description" aria-describedby="emailHelp">
 
         <div class="form-check">
-            <input class="form-check-input" type="checkbox" name="Completed" id="Completed">
+            <input class="form-check-input" type="checkbox" name="Completed" id="Completed" aria-describedby="emailHelp">
             <label class="form-check-label" for="Completed">Completada</label>
         </div>
     
-    <button type="submit">Guardar</button>
-    <a href="index.php">Cancelar</a>    
+    <button type="submit" class="btn btn-primary">Guardar</button>
+    <a class="btn btn-danger" href="index.php">Cancelar</a>      
         
     </div>
 
+    <?php
+    require_once("../../view/head/footer.php");
+?>
