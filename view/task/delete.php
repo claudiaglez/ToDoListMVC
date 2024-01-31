c90ba37d1ff8d414a84e72086b9fa3d50f1908f4
Claudia González <claudia.glezg@gmail.com>
2024-01-31 21:41:47 +0100

    update delete

--- view/task/delete.php
+++ view/task/delete.php
@@ -1,10 +1,13 @@
 <?php
 
 use Controllers\ToDoController;
-
+require_once("./view/head/head.php");
 require_once("./controllers/ToDoController.php");
 
 $object = new ToDoController;
 $object->delete($_GET{'id'});
 
+?>
+<?php
+require_once("./view/head/footer.php");
 ?>
