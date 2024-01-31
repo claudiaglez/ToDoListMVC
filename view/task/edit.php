2bcea2e42f1737a6b2801fe37c3f66cc7d17955b
Claudia González <claudia.glezg@gmail.com>
2024-01-31 21:43:36 +0100

    update edit

--- view/task/edit.php
+++ view/task/edit.php
@@ -1,5 +1,6 @@
 <?php
 use Controllers\ToDoController;
+require_once("./view/head/head.php");
 require_once("./controllers/ToDoController.php");
 
 $object = new ToDoController();
@@ -10,28 +11,29 @@ $date = $object->show($_GET['id']);
 
 <form action="update.php" method="post" autocomplete="off">
     <h2>Editar Tarea</h2>
-  <div>
-    <label>Id</label>
-    <div>
-      <input type="text" name="id" readonly id="staticEmail" value="<?= $date["id"]?>">
+  <div class="mb-3 row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Id</label>
+    <div class="col-sm-10">
+      <input type="text" name="id" readonly class="form-control-plaintext" id="staticEmail" value="<?= $date["id"]?>">
     </div>
   </div>
-  <div>
-    <label>Tarea</label>
-    <div>
-      <input type="text" name="Title"id="inputPassword" value="<?= $date["Title"]?>">
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Tarea</label>
+    <div class="col-sm-10">
+      <input type="text" name="Title" class="form-control" id="inputPassword" value="<?= $date["Title"]?>">
     </div>
   </div>
-  <div>
-    <label>Descripción</label>
-    <div>
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Descripción</label>
+    <div class="col-sm-10">
       <input type="text" name="Description" class="form-control" id="inputPassword" value="<?= $date["Description"]?>">
     </div>
   </div>
-  
-    <input type="submit" value="Actualizar">
-    <a href="show.php?id=<?= $date["id"]?>">Cancelar</a>
+    <input type="submit" value="Actualizar" class="btn btn-success">
+    <a href="show.php?id=<?= $date["id"]?>" class="btn btn-danger">Cancelar</a>
   </div>
 </form>
 
-
+<?php
+require_once("./view/head/footer.php");
+?>
