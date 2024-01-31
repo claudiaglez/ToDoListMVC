eac93b0ef99e9ce3fcf1f86a50ea112843a900f3
Claudia González <claudia.glezg@gmail.com>
2024-01-31 21:01:30 +0100

    add controllers

--- controllers/ToDoController.php
+++ controllers/ToDoController.php
@@ -4,8 +4,39 @@ use config\Database;
 use Exception;
 
 require "vendor/autoload.php";
+require_once("../model/ToDoModel.php");
 
+class ToDoController{
+    private $model;
+    public function __construct()
+    {
+        $this->model = new ToDoModel();
+    }
 
+    public function store($Title, $Description){
+        $id = $this->model->insert($Title, $Description);
+        return ($id!=false) ? header("Location: show.php?id=".$id) : header("Location:create.php");
+    }
+
+    public function show($id){
+        return ($this->model->show($id) != false) ? $this->model->show($id) : header("Location:index.php");
+    }
+
+    public function index(){
+        return ($this->model->index()) ? $this->model->index() : false;
+    }
+
+    public function update($id, $Title, $Description){
+        return ($this->model->update($id,$Title, $Description) != false) ? header("Location:show.php?id=".$id) : header("Location:index.php");
+        }
+
+    public function delete($id){
+        return ($this->model->delete($id)) ? header("Location:index.php") : header("Location:show.php?id=".$id) ;
+            }
+
+    }
+
+}
 
 
 ?>
