1ab1c34b81589331197a54c45eb80dee74194f30
Claudia González <claudia.glezg@gmail.com>
2024-02-01 13:07:15 +0100

    change background

--- config/Database.php
+++ config/Database.php
@@ -3,13 +3,13 @@
 class Database {
     private $host = "localhost";
     private $dbname = "to_do_list";
-    private $user = "root";
+    private $username = "root";
     private $password = "";
     private $PDO;
 
     public function connection(){
         try {
-            $this->PDO = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname,$this->user,$this->password);
+            $this->PDO = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname,$this->username,$this->password);
             return $this->PDO;
         } catch (PDOException $e) {
             return $e->getMessage();
